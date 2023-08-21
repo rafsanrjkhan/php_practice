668edc4ce304ddd00624a8a294688eee4d5c8c8a
rafsanrjkhan <rjkhan104@gmail.com>
2023-08-21 22:19:08 +0600

    array and assiciate arrayt

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+echo "<br>"; 
+echo "<hr>"; 
+
+echo "Data Type - Associate Array";
+echo "<br>"; 
+$array["foo"] = "bar";
+$array[43] = "hello";
+echo $array["foo"];
+echo "<br>"; 
+echo $array[43];
+
+echo "<br>"; 
+echo "<hr>"; 
+
+echo "Data Type - Array";
+echo "<br>"; 
+$bar = ["A", true, 123=>5];
+echo $bar[0];
+echo "<br>"; 
+echo $bar[1] ;
+echo "<br>"; 
+echo $bar[123] ;
